2e11dbcdd9c19321d4349a782b3f7347f3e5afd2
Mohamed Osama <mossama370@gmail.com>
2024-06-12 18:13:15 +0300

    AUTH DONE

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -8,5 +8,7 @@ class AppImages {
 
   /// ******************************************** ///
   static const String whiteLogo = '$_svg/white_logo.svg';
+  static const String carrotLogo = '$_svg/carrot_logo.svg';
+  static const String password = '$_svg/password.svg';
 
 }
