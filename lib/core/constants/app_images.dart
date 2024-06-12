d9ab5d8532966343ae8c2bf3dcfa9de7709f331d
George Abd-Elmesseh <76843136+George-Abdelmessh@users.noreply.github.com>
2024-06-12 19:06:15 +0300

    Update app_images.dart

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -1,26 +1,18 @@
 class AppImages {
-
   static const String _images = 'assets/images';
   static const String _svg = 'assets/svg';
 
   /// ******************************************** ///
   static const String splash = '$_images/splash.png';
   static const String onBoarding = '$_images/onboarding.png';
-
+  static const String apple = '$_images/apple.png';
+  static const String pulses = '$_images/pulses.png';
   static const String vector = '$_images/Vector.png';
 
   /// ******************************************** ///
   static const String whiteLogo = '$_svg/white_logo.svg';
-
-  static const String apple = '$_images/apple.png';
-  static const String pulses = '$_images/pulses.png';
-
-  /// ******************************************** ///
-  static const String whiteLogo = '$_svg/white_logo.svg';
   static const String carrotLogo = '$_svg/carrot_logo.svg';
-
   static const String password = '$_svg/password.svg';
-
   static const String explore = '$_svg/explore.svg';
   static const String shop = '$_svg/shop.svg';
   static const String cart = '$_svg/cart.svg';
@@ -28,6 +20,4 @@ class AppImages {
   static const String account = '$_svg/account.svg';
   static const String location = '$_svg/location.svg';
   static const String search = '$_svg/search.svg';
-
 }
-
